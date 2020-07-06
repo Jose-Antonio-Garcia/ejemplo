51612fff579180facb646fbc990a05d94fb78d10
jose5717 <jose.garcia@uaem.mx>
2020-07-06 09:58:44 -0500

    se Agrego codigo para la carga por OTA ( over the air updates)

--- ejemplo1/src/main.cpp
+++ ejemplo1/src/main.cpp
@@ -2,17 +2,107 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+//Librerias para OTA
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+//Librerias para OTA
+
+#if defined(ESP32_RTOS) && defined(ESP32)
+void taskOne( void * parameter )
+{
+  ArduinoOTA.handle();
+  delay(3500);
+}
+#endif
+
+const char *ssid="Ubee16F8-2.4G";
+const char *passwrd="5F99F616F8";
+
+
+void setupOTA(const char* nameprefix) {
+  const int maxlen = 40;
+  char fullhostname[maxlen];
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  snprintf(fullhostname, maxlen, "%s-%02x%02x%02x", nameprefix, mac[3], mac[4], mac[5]);
+  ArduinoOTA.setHostname(fullhostname);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, passwrd);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+  .onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  })
+  .onEnd([]() {
+    Serial.println("\nEnd");
+  })
+  .onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  })
+  .onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+
+  Serial.println("OTA Initialized");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+#if defined(ESP32_RTOS) && defined(ESP32)
+  xTaskCreate(
+    ota_handle,          /* Task function. */
+    "OTA_HANDLE",        /* String with name of task. */
+    10000,            /* Stack size in bytes. */
+    NULL,             /* Parameter passed as input of the task */
+    1,                /* Priority of the task. */
+    NULL);            /* Task handle. */
+#endif
+}
+
+
+unsigned long entry;
+
 
 class conexionWeb{
   private:
 
   public:
     conexionWeb(const char *_SSID, const char* _PASSWORD ){
-      WiFi.begin(_SSID, _PASSWORD);
+      /* WiFi.begin(_SSID, _PASSWORD);
       while(WiFi.status()!= WL_CONNECTED){
           delay(500);
           Serial.print("...");
-      }
+      } */
     }
 
 }; 
@@ -42,10 +132,7 @@ public:
 
   void setLocalTime()
   {
-    
-    if (!getLocalTime(&timeinfo))
-    {
-    }
+    if (getLocalTime(&timeinfo))
     strftime(timeStringBuff, sizeof(timeStringBuff), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
   }
 };
@@ -53,26 +140,31 @@ conexionWeb *webInterface;
 DateTime *reloj;
 StaticJsonDocument<512> horaActual;
 
-
-const char *ssid="Ubee16F8-2.4G";
-const char *passwrd="5F99F616F8";
-
-
 void setup() {
   Serial.begin(115200);
+  setupOTA("TemplateSketch");
   webInterface= new conexionWeb(ssid,passwrd);
   reloj=new DateTime();
   
+  pinMode(2,OUTPUT);
 }
-int suma=0;
+int sumatoria=0;
 void loop() {
-  suma++;
+   #ifndef ESP32_RTOS
+    ArduinoOTA.handle();
+  #endif
+  sumatoria++;
   horaActual.clear();
   reloj->getTime();
   horaActual["hora"]=reloj->timeStringBuff;
-  horaActual["Sumatoria"]=suma;
-  serializeJson(horaActual,Serial);
-  Serial.println("");
-  delay(1000);
+  horaActual["Sumatoria"]=sumatoria;
+  delay(100);
+  digitalWrite(2,HIGH);
+  delay(100);
+  digitalWrite(2,LOW);
+
+ // serializeJson(horaActual,Serial);
+ // Serial.println(""); 
+ 
 }
 
