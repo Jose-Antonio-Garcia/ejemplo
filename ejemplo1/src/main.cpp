95f1502277a0a5485654019f210a198605f1515a
jose5717 <jose.garcia@uaem.mx>
2020-07-13 08:55:05 -0500

    se organizo el codigo y se añadio el consumo de APi para insertar datos

--- ejemplo1/src/main.cpp
+++ ejemplo1/src/main.cpp
@@ -6,165 +6,61 @@
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+
+#include "../lib/ota.h"
+#include "../lib/interfazweb.h"
+#include "../lib/reloj.h"
+
 //Librerias para OTA
 
-#if defined(ESP32_RTOS) && defined(ESP32)
-void taskOne( void * parameter )
-{
-  ArduinoOTA.handle();
-  delay(3500);
-}
-#endif
-
-const char *ssid="Ubee16F8-2.4G";
-const char *passwrd="5F99F616F8";
-
-
-void setupOTA(const char* nameprefix) {
-  const int maxlen = 40;
-  char fullhostname[maxlen];
-  uint8_t mac[6];
-  WiFi.macAddress(mac);
-  snprintf(fullhostname, maxlen, "%s-%02x%02x%02x", nameprefix, mac[3], mac[4], mac[5]);
-  ArduinoOTA.setHostname(fullhostname);
-  
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, passwrd);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-
-  // Port defaults to 3232
-  // ArduinoOTA.setPort(3232);
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  ArduinoOTA
-  .onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
-  })
-  .onEnd([]() {
-    Serial.println("\nEnd");
-  })
-  .onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  })
-  .onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-
-  ArduinoOTA.begin();
-
-  Serial.println("OTA Initialized");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-#if defined(ESP32_RTOS) && defined(ESP32)
-  xTaskCreate(
-    ota_handle,          /* Task function. */
-    "OTA_HANDLE",        /* String with name of task. */
-    10000,            /* Stack size in bytes. */
-    NULL,             /* Parameter passed as input of the task */
-    1,                /* Priority of the task. */
-    NULL);            /* Task handle. */
-#endif
-}
-
-
-unsigned long entry;
-
-
-class conexionWeb{
-  private:
-
-  public:
-    conexionWeb(const char *_SSID, const char* _PASSWORD ){
-      /* WiFi.begin(_SSID, _PASSWORD);
-      while(WiFi.status()!= WL_CONNECTED){
-          delay(500);
-          Serial.print("...");
-      } */
-    }
-
-}; 
-
-class DateTime
-{
-private:
-  int defaultTime;
-  struct tm timeinfo;
-  const char *ntpServer = "pool.ntp.org";
-  const long gmtOffset_sec = 0;
-  const int daylightOffset_sec = 0;
-
-public:
-  char timeStringBuff[20];
-  DateTime()
-  {
-    defaultTime = 0;
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-    getTime();
-  }
-
-  void getTime()
-  {   
-    setLocalTime();
-  }
-
-  void setLocalTime()
-  {
-    if (getLocalTime(&timeinfo))
-    strftime(timeStringBuff, sizeof(timeStringBuff), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
-  }
-};
 conexionWeb *webInterface;
+
+
 DateTime *reloj;
 StaticJsonDocument<512> horaActual;
+StaticJsonDocument<512> post_ejemplo;
+StaticJsonDocument<1024> get_ejemplo;
 
-void setup() {
+const char *urlPost = "http://192.168.0.21/esp32-api/public/api/sensores";
+const char *geturl = "http://192.168.0.21/app/public/api/resetpassword";
+
+void setup()
+{
   Serial.begin(115200);
   setupOTA("TemplateSketch");
-  webInterface= new conexionWeb(ssid,passwrd);
-  reloj=new DateTime();
-  
-  pinMode(2,OUTPUT);
+
+  webInterface = new conexionWeb(ssid, passwrd);
+  reloj = new DateTime();
+
+  pinMode(2, OUTPUT);
 }
-int sumatoria=0;
-void loop() {
-   #ifndef ESP32_RTOS
-    ArduinoOTA.handle();
-  #endif
+int sumatoria = 0;
+void loop()
+{
+#ifndef ESP32_RTOS
+  ArduinoOTA.handle();
+#endif
   sumatoria++;
+
   horaActual.clear();
+  post_ejemplo.clear();
   reloj->getTime();
-  horaActual["hora"]=reloj->timeStringBuff;
-  horaActual["Sumatoria"]=sumatoria;
-  delay(100);
-  digitalWrite(2,HIGH);
-  delay(100);
-  digitalWrite(2,LOW);
 
- // serializeJson(horaActual,Serial);
- // Serial.println(""); 
- 
+  horaActual["hora"] = reloj->timeStringBuff;
+  horaActual["Sumatoria"] = sumatoria;
+
+  post_ejemplo["sensor"] = "temperatura";
+  post_ejemplo["valor"] = random(30);
+
+  webInterface->webPOST(post_ejemplo, urlPost);
+  //  webInterface->webGET(geturl);
+
+  deserializeJson(get_ejemplo, webInterface->POSTresponse);
+  serializeJsonPretty(get_ejemplo, Serial);
+  Serial.println("");
+  Serial.println("");
+  Serial.println("");
+  Serial.println("");
+  
+  // serializeJson(horaActual,Serial);
 }
-
